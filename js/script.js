689773eb734759765b3aa0fd92299b7f6f8ee9c6
Ali (the one) <tagazali@gmail.com>
2020-04-13 17:12:42 +0200

    css : ajout de variable et modification du style

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-const addButton = document.getElementById('submit-touit')
-const touitText = document.getElementById('touit-text')
-const touitList = document.getElementById('touit-list')
+const addButton = document.getElementById('submit-touit');
+const touitText = document.getElementById('touit-text');
+const touitList = document.getElementById('touit-list');
+const touiDelete = document.getElementById('delete-touit');
 
 
-addButton.addEventListener("click", function() {
+
+function addNote() {
     const newTouit = document.createElement('li');
     newTouit.textContent = touitText.value;
     touitList.appendChild(newTouit);
-});
+};
+
+addButton.addEventListener('click', addNote)
