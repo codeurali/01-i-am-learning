388f1abfc07002de16cf7329ca5b5b06f2cea227
Ali (the one) <tagazali@gmail.com>
2020-04-16 10:32:36 +0200

    Ajout de la fonction ajoutant un message avec un pseudo.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-const addButton = document.getElementById('submit-touit');
 const touitText = document.getElementById('touit-text');
 const touitList = document.getElementById('touit-list');
-const touiDelete = document.getElementById('delete-touit');
+const touitDelete = document.getElementById('delete-touit');
+const myForm = document.getElementById('form-touit')
+const name = document.getElementById('name')
 
 
-
-function addNote() {
-    const newTouit = document.createElement('li');
+function addNote(event) {
+// Prévient le rechargement de la page à chaque submit
+    event.preventDefault();
+// Création des éléments du DOM phantôme
+    const touitLi = document.createElement('li');
+    const newName = document.createElement('small');
+    const newTouit = document.createElement('p');
+// Relie les informations du DOM et du DOM phantôme 
+    touitLi.className = 'touit'; 
+    newName.textContent = name.value;
     newTouit.textContent = touitText.value;
+// Incrémente les éléments du DOM phantôme dans le DOM 'réél'
+    touitList.appendChild(newName)
     touitList.appendChild(newTouit);
+    touitList.appendChild(touitLi);
 };
 
-addButton.addEventListener('click', addNote)
+myForm.addEventListener('submit', addNote);
